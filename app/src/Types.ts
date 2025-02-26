a0f964cc5994db3089a4de4f0fe0ca7417f9cfb9
JYKIM317 <augu202@naver.com>
2025-02-27 00:29:13 +0900

    feat: Post 관련 type 정의

---
+++ app/src/Types.ts
@@ -0,0 +1,35 @@
+/*post*/
+type MediaType = "image" | "video";
+
+export type PostMediaStructure = {
+  type: MediaType;
+  url: string;
+};
+
+export type PostInfo = {
+  user_name: string;
+  badge: string | null;
+  user_id: string;
+  create_at: Date;
+};
+
+export type PostBottom = {
+  comment_count: number;
+  like_count: number;
+  scrap_count: number;
+};
+
+export type Post = {
+  post_id: string;
+  user_id: string;
+  user_name: string;
+  profile_image: string; // url
+  badge: string | null;
+  create_at: Date;
+  content: {
+    text: string;
+    media: PostMediaStructure[];
+  };
+};
+
+/**/
