6cb0136bdb1e750e1d973dd684b8db6876a3856e
JYKIM317 <augu202@naver.com>
2025-06-01 00:01:41 +0900

    refactor: Refactoring Content component without button
    포스트 페이지로 이동하기 위한 상호작용을 위해 포스트를 버튼으로 감쌌던
    부분에 button 하위 button 중첩 경고를 해소하기 위해 button 태그를 div
    태그로 변경하고, role을 button으로 변경하고, cursor-pointer를 추가하였습니다.
    
    추가로 포스트를 눌러 포스트페이지로 이동할 때 API 호출 횟수를 줄이고,
    조금 더 빠른 속도로 데이터를 렌더링하기 위해 네비게이터로 Post data를
    전달하도록 옵션을 추가하였습니다.

--- app/src/components/post/Content.tsx
+++ app/src/components/post/Content.tsx
@@ -21,50 +21,53 @@ function Content(post: Post) {
   const handleClick = (e) => {
     e.stopPropagation();
     if (!isDragging) {
-      navigator(`/post/${post.post_id}`);
+      navigator(`/post/${post.post_id}`, { state: post });
     }
   };
 
   return (
-    <div>
-      <button
-        className="!p-0"
-        onClick={() => navigator(`/post/${post.post_id}`)}
-      >
-        <div className="flex justify-start items-start gap-2 rounded-xs py-3 px-8 hover:bg-gray-100">
-          <MiniProfile user_id={post.user_id} img_url={post.profile_image} />
-          <div className="w-full flex flex-col justify-start gap-2">
-            <PostInfoBar {...(post as PostInfo)} />
-            <p
-              className="line-clamp-6 select-text cursor-text"
-              style={{ whiteSpace: "pre-wrap" }}
-              onMouseDown={handleMouseDown}
-              onMouseMove={handleMouseMove}
-              onClick={handleClick}
-            >
-              {post.content.text}
-            </p>
-            {mediaCount >= 1 && (
-              <MediaContent
-                contents={post.content.media}
-                post_id={post.post_id}
-              />
-            )}
-            <div className="w-full flex flex-wrap gap-2 item-start">
-              {post.content.tag.map((text, idx) => (
-                <div key={`${post.post_id}${text}${idx}`} className="relative">
-                  <Tag content={text} />
-                </div>
-              ))}
-            </div>
-            <PostBottomBar
-              comment_count={post.comment_count}
-              like_count={post.like_count}
-              scrap_count={post.scrap_count}
+    <div
+      role="button"
+      className="cursor-pointer"
+      onClick={() =>
+        navigator(`/post/${post.post_id}`, {
+          state: post,
+        })
+      }
+    >
+      <div className="flex justify-start items-start gap-2 rounded-xs py-3 px-8 hover:bg-gray-100">
+        <MiniProfile user_id={post.user_id} img_url={post.profile_image} />
+        <div className="w-full flex flex-col justify-start gap-2">
+          <PostInfoBar {...(post as PostInfo)} />
+          <p
+            className="line-clamp-6 select-text cursor-text"
+            style={{ whiteSpace: "pre-wrap" }}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onClick={handleClick}
+          >
+            {post.content.text}
+          </p>
+          {mediaCount >= 1 && (
+            <MediaContent
+              contents={post.content.media}
+              post_id={post.post_id}
             />
+          )}
+          <div className="w-full flex flex-wrap gap-2 item-start">
+            {post.content.tag.map((text, idx) => (
+              <div key={`${post.post_id}${text}${idx}`} className="relative">
+                <Tag content={text} />
+              </div>
+            ))}
           </div>
+          <PostBottomBar
+            comment_count={post.comment_count}
+            like_count={post.like_count}
+            scrap_count={post.scrap_count}
+          />
         </div>
-      </button>
+      </div>
     </div>
   );
 }
