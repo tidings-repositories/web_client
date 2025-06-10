706093e06eb8cc45c26f769917beecea0accea95
JYKIM317 <augu202@naver.com>
2025-06-10 20:54:34 +0900

    refactor: Refactoring Sign Component
    Add Signin word in component

--- app/src/components/sign/Sign.tsx
+++ app/src/components/sign/Sign.tsx
@@ -4,6 +4,7 @@ import * as l10n from "i18next";
 function Sign() {
   return (
     <div className="flex flex-col gap-8 justify-center px-10 py-4">
+      <p className="text-2xl">{l10n.t("signIn")}</p>
       <span>
         <p>
           {l10n.t("signNoti1")} <a>{l10n.t("userAgreement")}</a>{" "}
