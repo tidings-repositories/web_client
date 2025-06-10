2ceee357c15eec9048d3fb2d0ee6acd9a1b20f32
JYKIM317 <augu202@naver.com>
2025-06-10 17:33:05 +0900

    feat: Add Message layout and Direct message component
    메시지 리스트와 다이렉트 메시지를 표현하는 메시지 레이아웃과
    채팅의 뷰와 입력을 지원하는 다이렉트 메시지 컴포넌트를 구현하였습니다.

---
+++ app/src/layouts/Message.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { MessageUserSlotProps } from "../Types";
+import AppBar from "../components/public/AppBar";
+import Drawer from "../components/drawer/Drawer";
+import MessageAppBarItem from "../components/message/MessageAppBarItem";
+import RouterDrawerItem from "../components/drawer/RouterDrawerItem";
+import MessageList from "../components/message/MessageList";
+import DirectMessage from "../components/message/DirectMessage";
+
+import { createMockDM, createMockProfile } from "../../dev/mockdata";
+
+export default function Message() {
+  //TODO: replace to my user data state
+  const [userInfo, setUserInfo] = useState(createMockProfile());
+  const [messageList, setMessageList] = useState([] as MessageUserSlotProps[]);
+  const [selectedIdx, setSelectedDM] = useState(null as number | null);
+
+  const wideViewStandard = 1000;
+  const checkWideView = () => window.innerWidth > wideViewStandard;
+  const viewName = window.location.pathname.split("/").pop();
+
+  useEffect(() => {
+    //접근한 viewName에 따라서 userInfo로 해당 정보 받아오기
+    // - dm list or dm messages 만약 dm message라면  dm list 중에 있는지 확인하고 setSelectedDM
+
+    //TODO: fetch direct messages
+    setMessageList(Array.from({ length: 2 }, () => createMockDM()));
+  }, []);
+
+  return (
+    <div id="scaffold" className="w-screen h-screen mx-auto content-start">
+      <AppBar child={<MessageAppBarItem />} />
+      <Drawer child={<RouterDrawerItem />} />
+      {(checkWideView() && (
+        <div
+          id="message"
+          className="flex h-full justify-center px-4 pt-20 divide-x-2 divide-solid divide-gray-300"
+        >
+          <MessageList
+            directMessages={messageList}
+            selectedIdx={selectedIdx}
+            stateDispatcher={setSelectedDM}
+          />
+          <DirectMessage
+            directMessageInfo={
+              selectedIdx != null ? messageList[selectedIdx] : null
+            }
+            stateDispatcher={setSelectedDM}
+          />
+        </div>
+      )) || (
+        <div id="message" className="flex h-full justify-center px-4 pt-20">
+          {viewName == "message" ? (
+            <MessageList
+              directMessages={messageList}
+              selectedIdx={selectedIdx}
+              stateDispatcher={setSelectedDM}
+            />
+          ) : (
+            <DirectMessage
+              directMessageInfo={
+                selectedIdx != null ? messageList[selectedIdx] : null
+              }
+              stateDispatcher={setSelectedDM}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
