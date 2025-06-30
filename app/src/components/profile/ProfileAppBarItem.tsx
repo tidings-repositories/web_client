b164cc648fb0e8a80db114f6b43a55c2301a74b5
JYKIM317 <augu202@naver.com>
2025-07-01 03:05:22 +0900

    feat: Add profile appbar signIn/Up button
    프로필 페이지를 로그인 없이도 접근할 수 있도록 설정함에 따라 프로필
    페이지 앱바에 로그인 및 회원가입 버튼을 추가하였습니다.

--- app/src/components/profile/ProfileAppBarItem.tsx
+++ app/src/components/profile/ProfileAppBarItem.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 import IconButton from "../button/IconButton";
 import Logo from "../public/Logo";
 import MiniProfile from "../public/MiniProfile";
@@ -7,16 +6,15 @@ import NotificationDropdownItem from "../notification/NotificationDropdownItem";
 import Dropdown from "../public/Dropdown";
 import ReactDOM from "react-dom/client";
 import useUserDataStore from "../../store/UserDataStore";
+import Dialog from "../public/Dialog";
+import Sign from "../sign/Sign";
+import OutlineButton from "../button/OutlineButton";
+import * as l10n from "i18next";
 
 function ProfileAppBarItem() {
   const userId = useUserDataStore((state) => state.user_id);
   const profileImage = useUserDataStore((state) => state.profile_image);
   const navigator = useNavigate();
-
-  useEffect(() => {
-    //TODO: 로그인 상태 아니면 redirect
-    if (userId == null) navigator("/");
-  }, []);
 
   return (
     <div id="profile-appbar" className="w-full flex justify-between gap-4">
@@ -30,31 +28,48 @@ function ProfileAppBarItem() {
         <Logo />
       </div>
       {/*login feature or user feature*/}
-      <div className="content-center items-center flex gap-2">
-        <IconButton
-          icon="fa-solid fa-message"
-          size={18}
-          onPressed={() => navigator("/message")}
+      {userId ? (
+        <div className="content-center items-center flex gap-2">
+          <IconButton
+            icon="fa-solid fa-message"
+            size={18}
+            onPressed={() => navigator("/message")}
+          />
+          <IconButton
+            icon="fa-solid fa-pen-to-square"
+            size={18}
+            onPressed={() => navigator("/compose/post")}
+          />
+          <IconButton
+            icon="fa-solid fa-bell"
+            size={18}
+            onPressed={(e) =>
+              openNotificationDropdown(e, "notification-dropdown", userId)
+            }
+          />
+          <MiniProfile user_id={userId ?? ""} img_url={profileImage ?? ""} />
+        </div>
+      ) : (
+        <OutlineButton
+          fontSize="sm"
+          color="gray" //TODO: 컬러 테마 설정
+          text={`${l10n.t("signIn")}`}
+          radius={12}
+          onPressed={openDialog}
         />
-        <IconButton
-          icon="fa-solid fa-pen-to-square"
-          size={18}
-          onPressed={() => navigator("/compose/post")}
-        />
-        <IconButton
-          icon="fa-solid fa-bell"
-          size={18}
-          onPressed={(e) =>
-            openNotificationDropdown(e, "notification-dropdown", userId)
-          }
-        />
-        <MiniProfile user_id={userId ?? ""} img_url={profileImage ?? ""} />
-      </div>
+      )}
     </div>
   );
 }
 
 /*--------------*/
+function openDialog() {
+  const newDialog = document.createElement("div");
+  newDialog.id = `dialog-box`;
+  document.querySelector("body")!.appendChild(newDialog);
+  const root = ReactDOM.createRoot(newDialog);
+  root.render(<Dialog child={<Sign />} />);
+}
 
 function drawerClickEvent() {
   const drawer = document.getElementById("drawer")!;
