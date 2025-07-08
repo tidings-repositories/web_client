c6721b3eeb69d8e5fc8eae48713f97d30e7afc10
JYKIM317 <augu202@naver.com>
2025-07-08 18:05:22 +0900

    refactor: Add set slot component color
    슬롯 컴포넌트에 색상 옵션을 전달할 수 있도록 해 필요한 경우 색상 변경을
    수행하도록 변경하였습니다.

--- app/src/components/drawer/Slot.tsx
+++ app/src/components/drawer/Slot.tsx
@@ -1,10 +1,11 @@
 type SlotProps = {
   icon?: string;
   text: string;
+  color?: string;
   behavior: (e?: any) => void;
 };
 
-function Slot({ icon, text, behavior }: SlotProps) {
+function Slot({ icon, text, color = "black", behavior }: SlotProps) {
   return (
     <button className="!p-0 " onClick={behavior}>
       <div className="flex w-full gap-2 py-2 px-4 rounded-lg hover:bg-gray-100 items-center">
@@ -13,13 +14,15 @@ function Slot({ icon, text, behavior }: SlotProps) {
             className={icon}
             style={{
               fontSize: 16,
-              color: "gray",
+              color: color,
               width: 20,
               textAlign: "left",
             }}
           ></i>
         )}
-        <p className="text-lg">{text}</p>
+        <p className="text-lg" style={{ color: color }}>
+          {text}
+        </p>
       </div>
     </button>
   );
